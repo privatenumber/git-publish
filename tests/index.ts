3266043b6804c2d29d8c1f6da37d2fe93e9e75ce
Hiroki Osame <hiroki.osame@gmail.com>
2022-08-05 23:27:07 -0400

    test: cover publishing and dirty tree

--- tests/index.ts
+++ tests/index.ts
@@ -4,25 +4,70 @@ import { describe, expect } from 'manten';
 import { createFixture } from 'fs-fixture';
 
 const gitPublish = path.resolve('./dist/index.js');
+const repoShorthandPattern = /^git@github\.com:(.+)\.git$/;
 
-describe('Error cases', ({ test }) => {
-	test('Fails if not in git repository', async () => {
-		const fixture = await createFixture();
+describe('git-publish', async ({ describe, test }) => {
+	const { stdout: originUrl } = await execa('git', ['remote', 'get-url', 'origin']);
+	const repoShorthand = originUrl.match(repoShorthandPattern)!;
 
-		const gitPublishProcess = await execa(gitPublish, {
-			cwd: fixture.path,
-			reject: false,
+	describe('Error cases', ({ test }) => {
+		test('Fails if not in git repository', async () => {
+			const fixture = await createFixture();
+
+			const gitPublishProcess = await execa(gitPublish, {
+				cwd: fixture.path,
+				reject: false,
+			});
+
+			expect(gitPublishProcess.exitCode).toBe(1);
+			expect(gitPublishProcess.stderr).toBe('Error: Not in a git repository');
+
+			await fixture.rm();
 		});
 
-		expect(gitPublishProcess.stderr).toBe('Error: Not in a git repository');
+		test('Fails if no package.json found', async () => {
+			const fixture = await createFixture();
 
-		await fixture.rm();
+			await execa('git', ['init'], {
+				cwd: fixture.path,
+			});
+
+			const gitPublishProcess = await execa(gitPublish, {
+				cwd: fixture.path,
+				reject: false,
+			});
+
+			expect(gitPublishProcess.exitCode).toBe(1);
+			expect(gitPublishProcess.stderr).toBe('Error: No package.json found in current working directory');
+
+			await fixture.rm();
+		});
+
+		test('Dirty working tree', async () => {
+			const fixture = await createFixture();
+
+			await execa('git', ['clone', originUrl, '.'], {
+				cwd: fixture.path,
+			});
+
+			await fixture.writeFile('package.json', '{}');
+
+			const gitPublishProcess = await execa(gitPublish, {
+				cwd: fixture.path,
+				reject: false,
+			});
+
+			expect(gitPublishProcess.exitCode).toBe(1);
+			expect(gitPublishProcess.stderr).toBe('Error: Working tree is not clean');
+
+			await fixture.rm();
+		});
 	});
 
-	test('Fails if no package.json found', async () => {
+	test('Publishes', async () => {
 		const fixture = await createFixture();
 
-		await execa('git', ['init'], {
+		await execa('git', ['clone', originUrl, '.'], {
 			cwd: fixture.path,
 		});
 
@@ -31,7 +76,8 @@ describe('Error cases', ({ test }) => {
 			reject: false,
 		});
 
-		expect(gitPublishProcess.stderr).toBe('Error: No package.json found in current working directory');
+		expect(gitPublishProcess.exitCode).toBe(0);
+		expect(gitPublishProcess.stdout).toMatch(`npm i '${repoShorthand[1]}#npm/develop'`);
 
 		await fixture.rm();
 	});
