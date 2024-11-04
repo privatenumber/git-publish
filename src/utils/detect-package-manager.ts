a7dae4e9f1b6065e4878c2fb3b9196e31208f802
Hiroki Osame <hiroki.osame@gmail.com>
2024-11-04 15:20:51 +0900

    Published branch "develop"

--- src/utils/detect-package-manager.ts
+++
@@ -1,8 +0,0 @@
-import fs from 'node:fs/promises';
-
-export const detectPackageManager = () => Promise.any([
-	fs.access('package-lock.json').then(() => 'npm'),
-	fs.access('yarn.lock').then(() => 'yarn'),
-	fs.access('pnpm-lock.yaml').then(() => 'pnpm'),
-	fs.access('bun.lockb').then(() => 'bun'),
-]);
