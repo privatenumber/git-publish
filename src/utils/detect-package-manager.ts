3fce7fd7dc62971d24d850040973522b4463732d
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2025-09-01 10:37:57 +0100

    fix: add bun.lock support

--- src/utils/detect-package-manager.ts
+++ src/utils/detect-package-manager.ts
@@ -5,6 +5,7 @@ export const detectPackageManager = () => Promise.any([
 	fs.access('yarn.lock').then(() => 'yarn' as const),
 	fs.access('pnpm-lock.yaml').then(() => 'pnpm' as const),
 	fs.access('bun.lockb').then(() => 'bun' as const),
+	fs.access('bun.lock').then(() => 'bun' as const),
 ]).catch(
 	() => 'npm' as const, // If no lock files are found, default to npm
 );
